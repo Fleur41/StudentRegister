9fd9f0f3a23fd4d8ae3c2a81f717e5df9679b1e0
Fleur41 <Muriithikings456@gmail.com>
2024-04-11 12:53:34 +0300

    Created StudentDatabase.kt and StudentViewModel.kt

---
+++ app/src/main/java/com/okumu/studentregister/StudentViewModel.kt
@@ -0,0 +1,7 @@
+package com.okumu.studentregister
+
+import androidx.lifecycle.ViewModel
+import com.okumu.studentregister.db.StudentDao
+
+class StudentViewModel(private val dao: StudentDao): ViewModel() {
+}
